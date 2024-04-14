4aaa63c50ca4563dcb5520fa44a98265f89116e6
rauniyar01 <ashish.rauniyar@sintef.no>
2024-04-14 11:54:45 +0200

    Update App.js
    Token Fix

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ const authLink = setContext((_, { headers }) => {
     headers: {
       ...headers,
       //authorization: token ? `Bearer ${token}` : null,
-      authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjlQbDBadzZTZktFM1dUcGw5dGJOMSJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiXX0sImlzcyI6Imh0dHBzOi8vbmVtby1jaXRpZXMuZXUuYXV0aDAuY29tLyIsInN1YiI6IkE1ZW5MdTF0dEloZjlIS0wwcndIMmVMb1BhQTJtYUhsQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3F1ZXJ5aW5nLmNsb3VkLm5lbW8tY2l0aWVzLmV1L2FwaSIsImlhdCI6MTY2OTIxMTMyMCwiZXhwIjoxNjY5Mjk3NzIwLCJhenAiOiJBNWVuTHUxdHRJaGY5SEtMMHJ3SDJlTG9QYUEybWFIbCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.QlPlfJvSG_sTMVv9iIFZS6ERh1N7IF5wcuAOwRSJkoZF_Ql1eKFcpeYYIHwrfHi7yy8D8v4FBU9ufVK-Hr4deWqANC6fLUVSaKb6AA-L92bUnacXYHZtoZ882AGgWA21yl8B_fuyJulNd9wbj0jpjV3AH2QptVKZOmKEXBDdENsG2xB_oHI8LdNZH24_TZOze4SEbqbE-Q0juOtuECXydFSUm4Xgt_XiSjL6n8yQtLa8yikvpwG2qpmCg7zc900KryYzbW_RRkG7MBYt-ZFCMdEhzSYFdoaKtMM6_eiPY0f3CUUrlCE7yntD3slk1z996f0ewEavk1CNqd25ir3Tdg`
+      authorization: `Bearer Token`
     }
   }
 });
